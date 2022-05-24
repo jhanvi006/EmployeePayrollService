a466aeed392451aa353a4705778b165ef900199c
jhanvi006 <jhanvikankhara06@gmail.com>
2022-05-25 02:27:13 +0530

    Added: employee payroll read and write using console.

---
+++ src/main/java/com/bridgelabz/EmployeePayrollData.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+public class EmployeePayrollData {
+    int id;
+    String name;
+    double salary;
+    public EmployeePayrollData(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+    public String toString() {
+        return "Employee PayRoll Data: [Employee Id= "+id+", Name= "+name+", Salary= "+salary+"]";
+    }
+}
--- src/main/java/com/bridgelabz/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/EmployeePayrollService.java
@@ -1,7 +1,32 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class EmployeePayrollService {
+    Scanner consoleInputReader = new Scanner(System.in);
+    private List<EmployeePayrollData> employeePayrollList;
+    public EmployeePayrollService(ArrayList<EmployeePayrollData> employeePayrollList) {
+        this.employeePayrollList = employeePayrollList;
+    }
+    public void readEmployeePayrollData(){
+        System.out.println("Enter Employee ID.: ");
+        int id = consoleInputReader.nextInt();
+        System.out.println("Enter Employee name: ");
+        String name = consoleInputReader.next();
+        System.out.println("Enter Employee salary: ");
+        double salary = consoleInputReader.nextDouble();
+        employeePayrollList.add(new EmployeePayrollData(id, name, salary));
+    }
+    public void writeEmployeePayrollData(){
+        System.out.println("\nWriting Employee Payroll Data to console: \n"+employeePayrollList);
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Payroll Service System.");
+        ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
+        employeePayrollService.readEmployeePayrollData();
+        employeePayrollService.writeEmployeePayrollData();
     }
 }
